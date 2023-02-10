dc7d531734f511f6257c1c771b625d6921caeeff
Akolite98 <nnorompreciousakachukwu@gmail.com>
2023-02-10 17:31:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+const { application } = require('express');
+// const { moveMessagePortToContext } = require('worker_threads');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// MIDDLEWARES
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+//ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: ' fail',
+    message: `can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  //FOR LOCAL DATABASE CONNECTION
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}.......`);
+});
